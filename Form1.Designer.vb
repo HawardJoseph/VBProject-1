6337098394c51ab390faa686d3db20990e48ecf0
Haward Joseph <HJoseph@firstam.com>
2020-05-17 17:53:34 +0530

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.cmdButton1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'cmdButton1
+        '
+        Me.cmdButton1.Location = New System.Drawing.Point(562, 97)
+        Me.cmdButton1.Name = "cmdButton1"
+        Me.cmdButton1.Size = New System.Drawing.Size(188, 142)
+        Me.cmdButton1.TabIndex = 0
+        Me.cmdButton1.Text = "Click here"
+        Me.cmdButton1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(55, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(138, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Welcome program"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmdButton1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cmdButton1 As Button
+    Friend WithEvents Label1 As Label
+
+    Private Sub cmdButton1_Click(sender As Object, e As EventArgs) Handles cmdButton1.Click
+        MsgBox("Welcome to VB World")
+    End Sub
+End Class
